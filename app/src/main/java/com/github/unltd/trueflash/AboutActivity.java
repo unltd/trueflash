a43e7cc2a28fc4d727df31e83af231912371992b
Pavel Smirnov <psmirnov@devexperts.com>
2017-10-04 14:15:40 +0300

    Initial commit

---
+++ app/src/main/java/com/github/unltd/trueflash/AboutActivity.java
@@ -0,0 +1,36 @@
+package com.github.unltd.trueflash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import io.github.sanbeg.flashlight.R;
+
+public class AboutActivity extends Activity  {
+    
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+
+	public void onClick(View v) {
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.i_love_it:
+			intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=io.github.sanbeg.flashlight"));
+			startActivity(intent);
+			break;
+		case R.id.needs_work:
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/sanbeg/flashlight/issues/"));
+			startActivity(intent);
+			break;
+		case R.id.maybe_later:
+			//Go ahead and continue with your application
+			finish();
+			break;
+		}
+	}
+}
